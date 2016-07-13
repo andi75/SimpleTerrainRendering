70e6da375519b980f3bb4c8b6a4cd0e794f3be44
Andreas Umbach <umbach@gmail.com>
2016-07-13 11:24:38 +0200

    - added mouse grabbing and delta detection on mousedown for camera

--- SimpleTerrainOSX/TerrainViewOSX.swift
+++ SimpleTerrainOSX/TerrainViewOSX.swift
@@ -7,14 +7,31 @@
 //
 
 import Cocoa
+import CoreGraphics
 
 class TerrainViewOSX : NSOpenGLView
 {
+    override var acceptsFirstResponder: Bool { return true }
+    
     let renderer = TerrainRenderer()
+    
+    var mousePosition : CGPoint = CGPointMake(0, 0)
+    var mouseDown = false
     
     override func drawRect(dirtyRect: NSRect) {
         renderer.render(width: self.frame.width, height: self.frame.height)
         glFlush()
+    }
+    
+    override func viewDidMoveToWindow() {
+        // apparently all the following is unneeded
+//        self.window?.makeKeyWindow()
+//        if(self.window!.makeFirstResponder(self))
+//        {
+//            Swift.print("I should be first responder now")
+//            Swift.print("AcceptsFirstResponder: \(self.acceptsFirstResponder)")
+//        }
+        // self.window?.acceptsMouseMovedEvents = true
     }
     
     override func acceptsFirstMouse(theEvent: NSEvent?) -> Bool {
@@ -23,8 +40,32 @@ class TerrainViewOSX : NSOpenGLView
     
     override func mouseDown(theEvent: NSEvent) {
         Swift.print("mouse down")
+        // CGDisplayHideCursor(kCGDirectMainDisplay)
+        CGDisplayHideCursor(0)
+        CGAssociateMouseAndMouseCursorPosition(0)
     }
+    
+    override func mouseUp(theEvent: NSEvent) {
+        Swift.print("mouse up")
+        // CGDisplayShowCursor(kCGDirectMainDisplay)
+        CGAssociateMouseAndMouseCursorPosition(1)
+        CGDisplayShowCursor(0)
+    }
+    
+//    override func mouseMoved(theEvent: NSEvent) {
+//    }
+    
+    override func mouseDragged(theEvent: NSEvent) {
+        var dx : Int32 = 0, dy : Int32 = 0
+        CGGetLastMouseDelta(&dx, &dy)
+        Swift.print("delta: \(dx), \(dy)")
+    }
+    
     override func keyDown(theEvent: NSEvent) {
         Swift.print("key down")
+    }
+    
+    override func keyUp(theEvent: NSEvent) {
+        Swift.print("key up")
     }
 }
